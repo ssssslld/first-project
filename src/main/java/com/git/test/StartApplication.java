cbff8f88d0250c75cd22235afe4aa4befac9a5e0
liuzheng <zheng.liu@seres.cn>
2020-12-18 03:34:30 +0800

    修改内容

--- src/main/java/com/git/test/StartApplication.java
+++ src/main/java/com/git/test/StartApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StartApplication {
 
     public static void main(String[] args) {
+        System.out.println("作修改");
         SpringApplication.run(StartApplication.class, args);
-        System.out.println("成功创建了一个项目");
+        System.out.println("成功创建了一个项目，编辑文字");
     }
 }
